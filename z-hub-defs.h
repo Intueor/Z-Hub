9239c877c5fe03fc66ac8af467d3cfe0c88cc0a9
ivan <procyon1980@mail.ru>
2020-02-20 01:05:41 +0300

    Мелкая правка.

--- z-hub-defs.h
+++ z-hub-defs.h
@@ -11,7 +11,7 @@
 #define ENV_NAME_LEN					64
 #define SCH_NEXT_Z_SHIFT				1.0
 #define SCH_LINK_Z_SHIFT				0.01
-#define ENV_STEP_WAITING				500
+#define ENV_STEP_WAITING				1
 #define TOUCHED_GEOMETRY				0b0001
 #define TOUCHED_CONTENT					0b0010
 #define TOUCHED_NAME					0b0100
